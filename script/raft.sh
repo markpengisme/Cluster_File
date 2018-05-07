a5c1e2d4b91b9001b746dd172609958302a958c4
Mark Peng <markpengisme@gmail.com>
2018-05-08 01:40:45 +0800

    update raft.sh

--- script/raft.sh
+++ script/raft.sh
@@ -9,6 +9,6 @@ done
 for v in `seq 1 $NUM`
 do
 	echo "\n========raft $v start=======\n"
-	RUN="cd home/node$v && sh raft-init.sh && sh raft-start.sh"
+	RUN="cd home/node$v && sh run.sh"
 	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN"
 done
