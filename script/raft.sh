7b3d421cd07bb1405e94eb5db532ad05a9817ec6
Mark Peng <markpengisme@gmail.com>
2018-05-08 02:32:58 +0800

    update raft.sh

--- script/raft.sh
+++ script/raft.sh
@@ -1,14 +1,14 @@
 NUM=$1
-echo "Clean all node"
+echo "init all node"
 for v in `seq 1 $NUM`
 do
-	RUN="cd home/node$v && sh stop.sh"
+	RUN="cd home/node$v && sh stop.sh && ./raft-init.sh"
 	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN"
 done
 
 for v in `seq 1 $NUM`
 do
 	echo "\n========raft $v start=======\n"
-	RUN="cd home/node$v && sh run.sh"
-	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN"
+	RUN="cd home/node$v && ./raft-start.sh"
+	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN" &
 done
