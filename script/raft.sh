dcd519c993f44904208db6773b92b0a2e1e3abb3
Mark Peng <markpengisme@gmail.com>
2018-05-08 00:13:34 +0800

    update run.sh

--- script/raft.sh
+++ script/raft.sh
@@ -1,9 +1,9 @@
 NUM=$1
-echo "\n========clean all node start=======\n"
+echo "Clean all node"
 for v in `seq 1 $NUM`
 do
 	RUN="cd home/node$v && sh stop.sh"
-	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN" &
+	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$RUN"
 done
 
 for v in `seq 1 $NUM`
