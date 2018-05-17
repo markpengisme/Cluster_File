351149bdd8c0377cd6689acd58119eaa3088541d
Mark Peng <markpengisme@gmail.com>
2018-05-17 14:11:04 +0800

    fix bug

---
+++ node_empty/raft-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -u
+set -e
+
+echo "[*] Cleaning up temporary data directories"
+rm -rf qdata
+mkdir -p qdata/logs
+
+echo "[*] Configuring node"
+mkdir -p qdata/dd/{keystore,geth}
+cp permissioned-nodes.json qdata/dd/static-nodes.json
+cp permissioned-nodes.json qdata/dd/
+cp key qdata/dd/keystore
+cp nodekey qdata/dd/geth/nodekey
+geth --datadir qdata/dd init genesis.json
--- one_click_add_node.sh
+++ one_click_add_node.sh
@@ -110,29 +110,38 @@ do
 done
 
 
-##init dir/gensis.json/empty_passwords.txt/allkey/constellation-start.sh
+##generate permissioned-nodes.json
+for v in `seq 1 $TOTAL_NUM`
+do
+  eval IPTEMP_$v=$(kubectl get svc nodesvc$v | awk 'NR>1 {print $4}')
+  eval ENODE_$v=$(kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "cat /home/node/enode.key")
+  eval COMBINE="enode://"$(echo \$ENODE_$v)"@"$(echo \$IPTEMP_$v)":21000?discport=0\"&\"raftport=50400"
+  echo $COMBINE >> 123.txt
+done
+## sed
+#
+# add \" in head and \", in tail, 
+# delete last line last character,
+# add '[' in first line
+# add ']' in last line
+##
+sed -e 's/.*/"&",/' -e '$ s/.$//' -e '1i[' -e '$a]' 123.txt > node_empty/permissioned-nodes.json
+rm 123.txt
+
+
+## copy node_empty folder
+for v in `seq $TOTAL_NUM`
+do
+	kubectl cp -r node_empty \
+	$(kubectl get pods --selector=node=node$v | awk 'NR>1 {print $1}'):/home
+    echo "copy node folder ok"
+done
+
+
+##allkey/constellation-start.sh
 for v in `seq $(($EXIST_NUM+1)) $TOTAL_NUM`
 do
 	GENERATE_DIR="mkdir -p /home/node && cd /home/node"
-	GENERATE_GENSIS="echo '{
-	  \"alloc\": {},
-	  \"coinbase\": \"0x0000000000000000000000000000000000000000\",
-	  \"config\": {
-	    \"homesteadBlock\": 0,
-	    \"chainId\": 1,
-	    \"eip155Block\": null,
-	    \"eip158Block\": null,
-	    \"isQuorum\": true
-	  },
-	  \"difficulty\": \"0x0\",
-	  \"extraData\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
-	  \"gasLimit\": \"0xE0000000\",
-	  \"mixhash\": \"0x00000000000000000000000000000000000000647572616c65787365646c6578\",
-	  \"nonce\": \"0x0\",
-	  \"parentHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
-	  \"timestamp\": \"0x00\"
-	}' > genesis.json"
-	GENERATE_PASSWORD="echo '' > passwords.txt"
 	GENERATE_KEY='#!/bin/bash
 	bootnode -genkey nodekey \
 	&& bootnode -writeaddress -nodekey nodekey > enode.key \
@@ -163,81 +172,12 @@ do
 	CREATE_CONSTELLATION_START="echo '$GENERATE_CONSTELLATION_START' > constellation-start.sh && chmod 755 constellation-start.sh"
 	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c \
 		"$GENERATE_DIR && \
-		 $GENERATE_GENSIS && \
-		 $GENERATE_PASSWORD && \
 		 $GENERATE_KEY && \
 		 $CREATE_CONSTELLATION_START "
 done
 
-##generate permissioned-nodes.json
-for v in `seq 1 $TOTAL_NUM`
-do
-  eval IPTEMP_$v=$(kubectl get svc nodesvc$v | awk 'NR>1 {print $4}')
-  eval ENODE_$v=$(kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "cat /home/node/enode.key")
-  eval COMBINE="enode://"$(echo \$ENODE_$v)"@"$(echo \$IPTEMP_$v)"21000?discport=0\"&\"raftport=50400"
-  echo $COMBINE >> 123.txt
-done
-## sed
-#
-# add " in head and tail, 
-# delete last line last character,
-# add '[' in first line
-# add ']' in last line
-##
-sed -e 's/.*/"&",/' -e '$ s/.$//' -e '1i[' -e '$a]' 123.txt > permissioned-nodes.json
-rm 123.txt
-for v in `seq $TOTAL_NUM`
-do
-    PERMISSION=$(cat permissioned-nodes.json)
-    CREATE='cd home/node && echo '${PERMISSION}'> permissioned-nodes.json'
-    kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c "$CREATE"
-    echo "No.$v permissioned-nodes ok"
-done
 
-##init raft.init.sh/raft-start.sh/stop.sh
-for v in `seq $(($EXIST_NUM+1)) $TOTAL_NUM`
-do
-	GENERATE_RAFT_INIT='#!/bin/bash
-	set -u
-	set -e
 
-	echo "[*] Cleaning up temporary data directories"
-	rm -rf qdata
-	mkdir -p qdata/logs
-
-	echo "[*] Configuring node"
-	mkdir -p qdata/dd/{keystore,geth}
-	cp permissioned-nodes.json qdata/dd/static-nodes.json
-	cp permissioned-nodes.json qdata/dd/
-	cp key qdata/dd/keystore
-	cp nodekey qdata/dd/geth/nodekey
-	geth --datadir qdata/dd init genesis.json
-	'
-	GENERATE_RAFT_START='#!/bin/bash
-	set -u
-	set -e
-
-	mkdir -p qdata/logs
-	echo "[*] Starting Constellation Node"
-	./constellation-start.sh
-
-	echo "[*] Starting Ethereum Node"
-	ARGS="--raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
-	PRIVATE_CONFIG=qdata/c/tm.ipc nohup geth --datadir qdata/dd $ARGS --permissioned --raftport 50400 --rpcport 22000 --port 21000 --unlock 0 --password passwords.txt 2>>qdata/logs/log.log &
-	echo "Node configured. See 'qdata/logs' for logs, and run e.g. "geth attach qdata/dd/geth.ipc" to attach to the Geth node."
-	'
-	STOP_ALL='killall geth bootnode constellation-node'
-
-	CREATE_RAFT_INIT="echo '$GENERATE_RAFT_INIT' > raft-init.sh && chmod 755 raft-init.sh"
-	CREATE_RAFT_START="echo '$GENERATE_RAFT_START' > raft-start.sh && chmod 755 raft-start.sh"
-	CREATE_STOP_ALL="echo '$STOP_ALL' > stop.sh && chmod 755 stop.sh"
-
-	kubectl exec $(kubectl get pods --selector=node=node$v|  awk 'NR>1 {print $1}') -- bash -c \
-		"cd /home/node && \
-		 $CREATE_RAFT_INIT && \
-		 $CREATE_RAFT_START && \
-		 $CREATE_STOP_ALL "
-done
 
 ##blockchain development
 for v in `seq $(($EXIST_NUM+1)) $TOTAL_NUM`
