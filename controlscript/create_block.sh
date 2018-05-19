8b5e81cb70b74a9db5bd00beaaed1c7b908c2611
Mark Peng <markpengisme@gmail.com>
2018-05-19 16:11:32 +0800

    fix create_block.sh

--- controlscript/create_block.sh
+++ controlscript/create_block.sh
@@ -2,5 +2,5 @@ v=1
 POD_NAME=$(kubectl get pods --selector=node=node$v | awk 'NR>1 {print $1}')
 kubectl cp node_default/runscript.sh $POD_NAME:/home/node/runscript.sh
 kubectl cp node_default/script.js $POD_NAME:/home/node/script.js
-kubectl exec $POD_NAME -- bash -c "cd home/node && ./runscript.sh script.js"
+kubectl exec $POD_NAME -- bash -c "cd home/node && while true; do; sleep(1); ./runscript.sh script.js; done"
 	
